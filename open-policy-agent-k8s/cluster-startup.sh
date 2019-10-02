a175ead02576eb04c615df24989dc9466c67ddd6
Austin Heiman <atheimanksu@gmail.com>
2019-10-02 13:52:33 -0500

    testing webhook

---
+++ open-policy-agent-k8s/cluster-startup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+until kubectl cluster-info 2&>/dev/null; do
+  echo "Waiting for Kubernetes cluster startup..."
+  sleep 5
+done
+
+kubectl wait node --all --for=condition=Ready --timeout=3m
+
+source <(kubectl completion bash)
+
+cd /root/k8s
+
+clear
+
+echo "Cluster is ready! Cluster information:"
+
+kubectl cluster-info
+kubectl version --short
+
+# # kubeadm upgrade instructions from
+# # https://v1-15.docs.kubernetes.io/docs/tasks/administer-cluster/kubeadm/kubeadm-upgrade-1-15/
+# apt-get update -qy
+# # search for available versions: apt-cache policy kubeadm
+# TARGET_KUBE_VERSION="1.15.4"
+
+# # download latest binaries
+# apt-mark unhold kubeadm kubelet kubectl
+# apt-get install -qy kubeadm=${TARGET_KUBE_VERSION}-00 kubelet=${TARGET_KUBE_VERSION}-00 kubectl=${TARGET_KUBE_VERSION}-00
+# apt-mark hold kubeadm kubelet kubectl
+
+# # upgrade master
+# kubeadm upgrade plan
+# kubeadm upgrade apply --yes v${TARGET_KUBE_VERSION}
+
+# # restart kubelet on master
+# systemctl restart kubelet
