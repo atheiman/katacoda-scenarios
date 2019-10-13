d1b02b0759fdc4acadc87cfdfaeb2891271d77e6
clay <awsclay@amazon.com>
2019-10-13 13:59:21 -0500

    Fix validation

--- open-policy-agent-k8s-test/verifiers/opa-gatekeeper-install.sh
+++ open-policy-agent-k8s-test/verifiers/opa-gatekeeper-install.sh
@@ -1,1 +1,1 @@
-kubectl get pods -n gatekeeper-system | grep gatekeeper-controller-manager-0 | grep Running
+kubectl get pods -n gatekeeper-system | grep gatekeeper-controller-manager-0 | grep Running && echo "continue"
